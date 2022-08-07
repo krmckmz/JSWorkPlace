03f28ad04c88158c059edd76fef198b3872b4b03
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-08-07 19:39:37 +0300

    Added let var const differences

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-console.log("test");
+//VAR - LET - CONST
+
+// let age;
+// console.log(age);
+
+// age = 42;
+// console.log(42);
+
+// const name = "Kerem";
+// console.log(name);
+
+// name = "Mustafa";
+
+// let name = "Mustafa";
+// console.log(name);
+
+// name = "Kerem";
+// console.log(name);
+
+// let str = "";
+
+// for (let i = 0; i < 10; i++) {
+//     str = str.concat(i);
+//     console.log(str);
+// }
+
+// const myArr = [1,2,3];
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+// const student = {
+//     name:"Arin",
+//     age : 5
+// }
+// console.log(student);
+// Object.freeze();
+
+
+// student.name = "Elis";
+// console.log(student); 
+
+// var age = 40;
+// console.log(age);
+
+// age = 42;
+// console.log(42);
+
+//VAR --> global scope / functional scope
+
+// var x = 1;
+// console.log(x);
+
+// if (x == 1) {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+//LET --> block scope 
+
+// let x = 1;
+// console.log(x);
+
+// if (x == 1) {
+//     let x = 10;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+// let x = 10;
+// console.log(window.x);
+
+//HOISTING
+
+// var x = 10;
+
+console.log(x);
+
+const y = 20;
